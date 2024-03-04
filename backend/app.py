b67aaa44adcb26a04f59f2d1086eeceb5db790d4
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2024-03-03 21:35:52 -0800

    ✨ GPT-4 instruction parsing

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from openai import OpenAI
+
 import os
+import time
 
 from dotenv import load_dotenv
 
@@ -10,27 +12,62 @@ app = Flask(__name__)
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+# Waits for the run to complete
+def wait_on_run(run, thread):
+    while run.status == "queued" or run.status == "in_progress":
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
+        )
+        time.sleep(0.5)
+    return run
+
 @app.route("/api/audio", methods=["POST"])
 def hello_world():
-    audio_data = request.get_data(cache=False)
+    # Get audio data from client
+    audio_data = request.get_data(cache=False) 
 
-    byte_string = audio_data 
-
+    # Write audio data to file
     file_name = 'output_file.webm'
 
     with open(file_name, 'wb') as webm_file:
-        webm_file.write(byte_string)
+        webm_file.write(audio_data)
 
     app.logger.info(f'{file_name} successfully written.')
 
+    # Transcribe audio file
     audio_file= open(file_name, "rb")
     transcript = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
 
-    '''
-        Ideas:
-        - Use GPT-4 custom prompt to pull out what the user wants to do from their message and then use
-        some kind of messaging system in order to send that request to the client, where it would perform
-        said request
-    '''
+    transcript_text = transcript.text
 
-    return transcript.text
+    app.logger.info("Transcribed text")
+
+    # Get assistant and create a new thread
+    assistant = client.beta.assistants.retrieve(assistant_id="asst_JBoaod4vTwUk0eTuEQcjhaTt")
+
+    thread = client.beta.threads.create()
+
+    # Create a new message in the thread with the transcribed text
+    client.beta.threads.messages.create(
+      thread_id=thread.id,
+      role="user",
+      content=transcript_text
+    )
+
+    app.logger.info("Starting run")
+
+    # Run the assistant to parse the users request
+    run = client.beta.threads.runs.create(
+      thread_id=thread.id,
+      assistant_id=assistant.id,
+    )
+
+    run = wait_on_run(run, thread)
+
+    # Get the assistant's response, which is the instruction
+    messages = client.beta.threads.messages.list(thread_id=thread.id)
+    instruction = messages.data[0].content[0].text.value
+
+    # Return the instruction to the client
+    return instruction
